d9183a1720ef43be98db6290d49463d1aac7b60e
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-07-06 23:19:55 -0700

    working in the about componnets

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,7 +8,9 @@ const About = () => {
              <div className="ABout_text_Box"> 
                 <h3 className="about_Title">about  bistro Delight</h3>
                 <p className="ABout_p">
-                At Bistro Delight, we’re dedicated to delivering an exceptional dining experience. Nestled in the heart of the city, our restaurant offers a warm, inviting atmosphere perfect for any occasion. Our menu features a diverse array of dishes, blending local flavors with international flair, all crafted by our talented chefs using the freshest ingredients. 
+                At Bistro Delight, we’re dedicated to delivering an exceptional dining experience. Nestled in the heart of the city, our restaurant offers a warm, inviting atmosphere perfect for any occasion. Our menu features a diverse array of dishes, blending local flavors with international flair, all crafted by our talented chefs using the freshest ingredients.
+                </p>
+                <p className="ABout_p">
                 We believe in the art of fine dining and the joy of sharing delicious meals with loved ones. Whether you're enjoying a meal in our cozy dining area or opting for the convenience of our delivery service, we ensure that every dish is prepared to perfection and served with care.
                 </p>
              </div>
