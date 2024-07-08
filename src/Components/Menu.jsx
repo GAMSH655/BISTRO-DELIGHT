1504d1387cc991f5b4fa1c4b57f596bc2f8a69f3
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-07-08 06:51:58 -0700

    working

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -6,7 +6,12 @@ import Grilled_salmon from "../assets/Grilled.png"
 import Lemon_Sorbet from "../assets/Lemon.png"
 import Alfredo_Pasta from "../assets/Alfredo.png"
 import Greek_Salad from "../assets/Greek.png"
+import Table from "../assets/Table.png"
+import { FaStar } from 'react-icons/fa'
+import { useState } from 'react'
+
 const Menu = () => {
+  const [star , setStar] = useState(0)
   const MenuCollections= [
     {
       menuImg:Caesar_Salad,
@@ -53,15 +58,74 @@ const Menu = () => {
               MenuCollections.map(({menuImg ,menuInfo , menuName})=>{
                 return <div className="MenuCard">
                     <img src={menuImg} alt="" className='menuImg' />
-                    <div>
-                       <h3 className='menuName'>{menuName}</h3>
+                    <div className='Mainstar'>
+                    <h3 className='menuName'>{menuName}</h3>
+                       <div className="star">
+                       {
+                      Array.from({length:5}, (_ , i)=>{
+                        return <div key={i}>
+                        <FaStar key={i} onClick={()=>setStar(i + 1)}
+                        style={{ color :i < star ?  "FFAD33" : ""}}
+                />
+            </div>
+            })
+           } 
+                       </div>
                     </div>
                      <p className="menuInfo">{menuInfo}</p>
-                </div>
-                
-              })
+                </div>})
              }
-           </div>
+              
+              
+              <button className="view">view menu</button>
+              </div>
+              <div className="FloorGrid">
+                 <h3 className="why">why choose bristo</h3>
+                 <p className="">Reasons why Bristo Delight is the number one stop for classic dining</p>
+
+               <div className="AsideFlex">
+                 <img src={Table} alt="" />
+               </div>
+                <div className="asideGrid">
+                   <div className="GridCard">
+                     <span className="number">1</span>
+                     <h3 className="GridCardText">
+                     Exquisite Flavors
+                     </h3>
+                     <p className="gridParagraph">
+                     Our chefs craft each dish with the finest ingredients and culinary techniques to deliver unforgettable taste experiences
+                     </p>
+                   </div>
+                   <div className="GridCard">
+                     <span className="number">1</span>
+                     <h3 className="GridCardText">
+                     Exquisite Flavors
+                     </h3>
+                     <p className="gridParagraph">
+                     Our chefs craft each dish with the finest ingredients and culinary techniques to deliver unforgettable taste experiences
+                     </p>
+                   </div>
+                   <div className="GridCard">
+                     <span className="number">1</span>
+                     <h3 className="GridCardText">
+                     Exquisite Flavors
+                     </h3>
+                     <p className="gridParagraph">
+                     Our chefs craft each dish with the finest ingredients and culinary techniques to deliver unforgettable taste experiences
+                     </p>
+                   </div>
+                   <div className="GridCard">
+                     <span className="number">1</span>
+                     <h3 className="GridCardText">
+                     Exquisite Flavors
+                     </h3>
+                     <p className="gridParagraph">
+                     Our chefs craft each dish with the finest ingredients and culinary techniques to deliver unforgettable taste experiences
+                     </p>
+                   </div>
+                </div>
+              </div>
+           
     </div>
   )
 }
