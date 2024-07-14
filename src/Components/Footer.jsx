757d32bb25b32da6c7d2260e23434537ea8a58b2
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-07-14 13:37:55 -0700

    almost done with the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,15 +6,16 @@ import Apple from "../assets/Apple.png"
 import Google from "../assets/Google.png"
 const Footer = () => {
   return (
-    <div className='Footer'>
-        <div className="">
+    <div>
+           <div className='Footer'>
+        <div className="Footer_Logo_Container">
         <div className="logoConatiner">
          <h3 className='logoTExt'>
              bistro <span className='logoSpan'>delight</span>
          </h3>
            <img src={Corn} alt="" />
            </div>
-           <div className="footerIcon">
+           <div className="footerLinkDiv">
               <a href="" className="footerLink"> < FaFacebook /> </a>
               <a href="" className="footerLink"> <  FaLinkedin /> </a>
               <a href="" className="footerLink"> <  FaTwitter  /> </a>
@@ -31,16 +32,20 @@ const Footer = () => {
             <p className="footer_p">About us</p>
         </div>
         <div className="innerFooterDiv">
-            <h3 className="footerTitle">conatc us</h3>
+            <h3 className="footerTitle">contact us</h3>
             <p className="footer_p"> < FaEnvelope/> support@bristo.com</p>
             <p className="footer_p"> <FaPhone /> 0453173652451</p>
         </div>
         <div className="innerFooterDiv">
             <h3 className="footerTitle">download the app</h3>
-              <img src= {Google} alt="" />
+              <img src= {Google} alt="" className='footerImg' />
               <br /><br />
-              <img src= {Apple} alt="" />
+              <img src= {Apple} alt=""  className='footerImg' />
         </div>
+    </div>
+      <div className="ll">
+      <div className="line"></div>
+      </div>
     </div>
   )
 }
